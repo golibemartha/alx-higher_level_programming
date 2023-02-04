f128bb0aa05548ac3da449f6eef034656e7678cb
golibemartha <golibemartha@gmail.com>
2023-02-04 04:37:02 -0800

    fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A module that defines a square """
 
+
 class Square:
     """A class that represents a square"""
 
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 """My square module"""
 
 
